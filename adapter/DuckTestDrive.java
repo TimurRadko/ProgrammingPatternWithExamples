9f78dcfe250b7c6fad1743673d6c90593b11d0cf
Timur Radko <timurradko@gmail.com>
2020-07-26 18:49:31 +0300

    add implementation of Adapter Pattern

---
+++ adapter/DuckTestDrive.java
@@ -0,0 +1,31 @@
+package com.timurradko.adapter;
+
+public class DuckTestDrive {
+    public static void main(String[] args) {
+        MallardDuck duck = new MallardDuck();
+
+        WildTurkey turkey = new WildTurkey();
+        Duck turkeyAdapter = new TurkeyAdapter(turkey);
+
+        System.out.println("\nThe Turkey says...");
+        turkey.gobble();
+        turkey.fly();
+
+        System.out.println("\nThe TurkeyAdapter says...");
+        testDuck(duck);
+
+        System.out.println("\nThe TurkeyAdapter says...");
+        testDuck(turkeyAdapter);
+
+        System.out.println("\nThe DuckAdapter says...");
+        MallardDuck mallardDuck = new MallardDuck();
+        Turkey duckAdapter = new DuckAdapter(mallardDuck);
+        duckAdapter.fly();
+        duckAdapter.gobble();
+    }
+
+    static void testDuck(Duck duck) {
+        duck.quack();
+        duck.fly();
+    }
+}
---
+++ adapter/Turkey.java
@@ -0,0 +1,6 @@
+package com.timurradko.adapter;
+
+public interface Turkey {
+    void gobble();
+    void fly();
+}
